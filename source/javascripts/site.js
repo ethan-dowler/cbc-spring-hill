59e0ea9b274d29098e2cb6b4155acd7cd3ed6ac1
ethan-dowler <Ethan.dowler@daveramsey.com>
2018-05-25 22:43:45 -0500

    make belief sections toggle

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,21 @@
-// This is where it all goes :)
+(function() {
+  'use strict';
+
+  var toggle = function(e) {
+    if(e.target.dataset.toggle) {
+      var toggle = e.target.dataset.toggle;
+      var selector = '[data-target="' + toggle + '"]';
+      var target = document.querySelector(selector);
+      var baseClass = target.dataset.baseClass;
+      
+      if(target.className.indexOf('open') > -1) {
+        target.className = baseClass;
+      }
+      else {
+        target.className += ' open';
+      }
+    }
+  }
+
+  document.addEventListener('click', toggle);
+})()
