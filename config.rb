3825f8842463b485f1e5f52e7b602a2b1ab54e98
ethan-dowler <Ethan.dowler@daveramsey.com>
2019-01-19 18:54:15 -0600

    set the build dir to be the local repo for the github pages site

--- config.rb
+++ config.rb
@@ -4,6 +4,9 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+# set the local build path to the root of the Github Pages repo
+set :build_dir, 'build/cbcspringhill.github.io'
+
 require "sprockets/es6"
 activate :sprockets do |s|
   s.supported_output_extensions << ".es6"
